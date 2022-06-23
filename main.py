8f31cca2f4719f519f573b09959c946faa991af1
Cameron <85236636+Cswish24@users.noreply.github.com>
2022-06-22 21:49:08 -0500

    forgot to make commits, everything changed, initial prototype complete

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ class Task(db.Model):
     name = db.Column(db.String(30), nullable=False)
     description = db.Column(db.Text, nullable=False)
     due_date = db.Column(db.Date, nullable=True)
-    completion_notes = db.Column(db.Text, nullable=True)
+    notes = db.Column(db.Text, nullable=True)
     priority = db.Column(db.String(10), nullable=False)
     sub_tasks = db.relationship("SubTask", backref="task", lazy=True)
 
@@ -33,7 +33,7 @@ class SubTask(db.Model):
     name = db.Column(db.String(30), nullable=False)
     description = db.Column(db.Text, nullable=False)
     due_date = db.Column(db.Date, nullable=True)
-    completion_notes = db.Column(db.Text, nullable=True)
+    notes = db.Column(db.Text, nullable=True)
     priority = db.Column(db.String(10), nullable=False)
     task_id = db.Column(db.Integer, db.ForeignKey('Tasks.id'), nullable=False)
     sub_sub_tasks = db.relationship("SubSubTask", backref="subtask", lazy=True)
@@ -46,7 +46,7 @@ class SubSubTask(db.Model):
     name = db.Column(db.String(30), nullable=False)
     description = db.Column(db.Text, nullable=False)
     due_date = db.Column(db.Date, nullable=True)
-    completion_notes = db.Column(db.Text, nullable=True)
+    notes = db.Column(db.Text, nullable=True)
     priority = db.Column(db.String(10), nullable=False)
     subtask_id = db.Column(db.Integer, db.ForeignKey(
         'SubTasks.id'), nullable=False)
@@ -60,13 +60,21 @@ def home():
     tasks_due = Task.query.order_by(Task.due_date).limit(5).all()
     tasks_priority = Task.query.order_by(Task.priority).limit(5).all()
     tasks_recent = Task.query.order_by(desc(Task.id)).limit(5).all()
-    # tasks_recent_completed = Task.query.order_by(
-    #     Task.complete, desc(Task.completion_date)).limit(5).all()
+    tasks_recent_completed = Task.query.filter_by(
+        complete=True).limit(5).all()
     return render_template('index.html',
                            tasks_due=tasks_due,
                            tasks_priority=tasks_priority,
                            tasks_recent=tasks_recent,
-                           #  tasks_recent_completed=tasks_recent_completed,
+                           tasks_recent_completed=tasks_recent_completed,
+                           )
+
+
+@app.route('/all_tasks')
+def all_tasks():
+    all_tasks = Task.query.order_by(desc(Task.id)).all()
+    return render_template('all_tasks.html',
+                           tasks=all_tasks
                            )
 
 
@@ -83,11 +91,13 @@ def create_task():
             priority=request.form['priority'],
             due_date=due_date,
             complete=False,
+            notes=request.form['notes'],
         )
 
         db.session.add(new_task)
         db.session.commit()
-        task = Task.query.filter_by(name=request.form['name']).first()
+        task = Task.query.filter_by(
+            name=request.form['name']).order_by(desc(Task.id)).first()
 
         return redirect(url_for('task_view', task_id=task.id))
 
@@ -108,12 +118,13 @@ def create_subtask(task_id):
             due_date=due_date,
             complete=False,
             task_id=task_id,
+            notes=request.form['notes'],
         )
-        print(request.form['name'])
 
         db.session.add(new_task)
         db.session.commit()
-        subtask = SubTask.query.filter_by(name=request.form['name']).first()
+        subtask = SubTask.query.filter_by(
+            name=request.form['name']).order_by(desc(SubTask.id)).first()
         return redirect(url_for('subtask_view', subtask_id=subtask.id))
 
     return render_template('create_subtask.html', task=task)
@@ -131,40 +142,179 @@ def create_subsubtask(subtask_id):
             name=request.form['name'],
             description=request.form['description'],
             priority=request.form['priority'],
-            due_date=request.form['due_date'],
+            due_date=due_date,
             complete=False,
             subtask_id=subtask_id,
+            notes=request.form['notes'],
         )
-        print(request.form['name'])
 
         db.session.add(new_task)
         db.session.commit()
         subsubtask = SubSubTask.query.filter_by(
-            name=request.form['name']).first()
+            name=request.form['name']).order_by(desc(SubSubTask.id)).first()
         return redirect(url_for('subsubtask_view', subsubtask_id=subsubtask.id))
 
     return render_template('create_subsubtask.html', subtask=subtask)
 
 
-@app.route('/task_view/<int:task_id>')
+@app.route('/task_view/<int:task_id>', methods=['GET', 'POST'])
 def task_view(task_id):
-    print(task_id)
     task = Task.query.get(task_id)
-    subtasks = SubTask.query.filter_by(task_id=task_id).all()
-    return render_template('task_view.html', task=task, subtasks=subtasks)
+    if request.method == "POST":
+        task.complete = request.form['complete']
+        db.session.commit()
+        return redirect(url_for('notes', task_id=task.id))
+    subtasks = SubTask.query.filter_by(
+        task_id=task_id).order_by(SubTask.priority).all()
+    total_tasks = len(subtasks)
+    completed_tasks = 0
+    for subtask in subtasks:
+        if subtask.complete:
+            completed_tasks += 1
+
+    return render_template('task_view.html', completed_tasks=completed_tasks, total_tasks=total_tasks, task=task, subtasks=subtasks)
 
 
 @app.route('/subtask_view/<int:subtask_id>')
 def subtask_view(subtask_id):
     subtask = SubTask.query.get(subtask_id)
-    subsubtasks = SubSubTask.query.filter_by(subtask_id=subtask_id).all()
-    return render_template('subtask_view.html', subtask=subtask, subsubtasks=subsubtasks)
+    subsubtasks = SubSubTask.query.filter_by(
+        subtask_id=subtask_id).order_by(SubSubTask.priority).all()
+    total_tasks = len(subsubtasks)
+    completed_tasks = 0
+    for subsubtask in subsubtasks:
+        if subsubtask.complete:
+            completed_tasks += 1
+    return render_template('subtask_view.html', subtask=subtask, subsubtasks=subsubtasks, total_tasks=total_tasks, completed_tasks=completed_tasks)
 
 
 @app.route('/subsubtask_view/<int:subsubtask_id>')
 def subsubtask_view(subsubtask_id):
     subsubtask = SubSubTask.query.get(subsubtask_id)
     return render_template('subsubtask_view.html', subsubtask=subsubtask)
+
+
+@app.route('/edit_task/<int:task_id>', methods=['GET', 'POST'])
+def edit_task(task_id):
+    task = Task.query.get(task_id)
+    if request.method == 'POST':
+        due_date = request.form['due_date'].replace("-", "")
+        due_date = datetime.datetime.strptime(
+            due_date, '%Y%m%d')
+
+        task.name = request.form['name']
+        task.description = request.form['description']
+        task.priority = request.form['priority']
+        task.due_date = due_date
+        task.notes = request.form['notes']
+
+        db.session.commit()
+
+        return redirect(url_for('task_view', task_id=task.id))
+
+    return render_template('create_task.html', edit=True, task=task)
+
+
+@app.route('/edit_subtask/<int:subtask_id>', methods=['GET', 'POST'])
+def edit_subtask(subtask_id):
+    subtask = SubTask.query.get(subtask_id)
+    if request.method == 'POST':
+        due_date = request.form['due_date'].replace("-", "")
+        due_date = datetime.datetime.strptime(
+            due_date, '%Y%m%d')
+
+        subtask.name = request.form['name']
+        subtask.description = request.form['description']
+        subtask.priority = request.form['priority']
+        subtask.due_date = due_date
+        subtask.notes = request.form['notes']
+
+        db.session.commit()
+
+        return redirect(url_for('subtask_view', subtask_id=subtask.id))
+
+    return render_template('create_subtask.html', edit=True, subtask=subtask)
+
+
+@app.route('/edit_subsubtask/<int:subsubtask_id>', methods=['GET', 'POST'])
+def edit_subsubtask(subsubtask_id):
+    subsubtask = SubSubTask.query.get(subsubtask_id)
+    if request.method == 'POST':
+        due_date = request.form['due_date'].replace("-", "")
+        due_date = datetime.datetime.strptime(
+            due_date, '%Y%m%d')
+
+        subsubtask.name = request.form['name']
+        subsubtask.description = request.form['description']
+        subsubtask.priority = request.form['priority']
+        subsubtask.due_date = due_date
+        subsubtask.notes = request.form['notes']
+
+        db.session.commit()
+
+        return redirect(url_for('subsubtask_view', subsubtask_id=subsubtask.id))
+
+    return render_template('create_subsubtask.html', edit=True, subsubtask=subsubtask)
+
+
+@app.route('/task_delete/<int:task_id>')
+def task_delete(task_id):
+    db.session.delete(Task.query.get(task_id))
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
+@app.route('/subtask_delete/<int:subtask_id>')
+def subtask_delete(subtask_id):
+    db.session.delete(SubTask.query.get(subtask_id))
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
+@app.route('/subsub_delete/<int:subsubtask_id>')
+def subsubtask_delete(subsubtask_id):
+    db.session.delete(SubSubTask.query.get(subsubtask_id))
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
+@app.route('/task_complete/<int:task_id>')
+def task_complete(task_id):
+    task = Task.query.get(task_id)
+    if task.complete == False:
+        task.complete = True
+        db.session.commit()
+        return redirect(url_for('task_view', task_id=task_id))
+    if task.complete == True:
+        task.complete = False
+        db.session.commit()
+        return redirect(url_for('task_view', task_id=task_id))
+
+
+@app.route('/subtask_complete/<int:subtask_id>')
+def subtask_complete(subtask_id):
+    task = SubTask.query.get(subtask_id)
+    if task.complete == False:
+        task.complete = True
+        db.session.commit()
+        return redirect(url_for('subtask_view', subtask_id=subtask_id))
+    if task.complete == True:
+        task.complete = False
+        db.session.commit()
+        return redirect(url_for('subtask_view', subtask_id=subtask_id))
+
+
+@app.route('/subsubtask_complete/<int:subsubtask_id>')
+def subsubtask_complete(subsubtask_id):
+    task = SubSubTask.query.get(subsubtask_id)
+    if task.complete == False:
+        task.complete = True
+        db.session.commit()
+        return redirect(url_for('subsubtask_view', subsubtask_id=subsubtask_id))
+    if task.complete == True:
+        task.complete = False
+        db.session.commit()
+        return redirect(url_for('subsubtask_view', subsubtask_id=subsubtask_id))
 
 
 if __name__ == "__main__":
